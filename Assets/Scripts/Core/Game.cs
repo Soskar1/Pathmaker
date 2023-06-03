63b87eeec70be448020fe7c62791affde67f2729
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-06-03 14:44:15 +0300

    Game Over menu

--- Assets/Scripts/Core/Game.cs
+++ Assets/Scripts/Core/Game.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 namespace Pathmaker.Core
 {
     public class Game : MonoBehaviour
     {
         [SerializeField] private GameObject _ball;
+        [SerializeField] private GameObject _gameOverMenu;
 
         public Action EndingGame;
 
@@ -13,6 +15,10 @@ namespace Pathmaker.Core
         {
             _ball.SetActive(false);
             EndingGame?.Invoke();
+
+            _gameOverMenu.SetActive(true);
         }
+            
+        public void Restart() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
