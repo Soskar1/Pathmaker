8a7ea18a6f53c81c01d1c71be45a05cf49a1c600
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-06-03 12:07:40 +0300

    drawing

--- Assets/Scripts/Core/Drawing.cs
+++ Assets/Scripts/Core/Drawing.cs
@@ -4,10 +4,11 @@ namespace Pathmaker.Core
 {
     public class Drawing : MonoBehaviour
     {
+        [SerializeField] private Input _input;
         [SerializeField] private Line _linePrefab;
         [SerializeField] private float _updateDistance;
-        private Camera _camera;
 
+        private Camera _camera;
         private Line _currentLine;
         private Vector2 _lastPoint;
 
@@ -20,12 +21,24 @@ namespace Pathmaker.Core
             if (!_isDrawing)
                 return;
 
-            Debug.Log("Drawing");
+            Vector2 mousePosition = _input.GetMousePosition();
+            Vector2 worldPosition = _camera.ScreenToWorldPoint(mousePosition);
+
+            if (Vector2.Distance(worldPosition, _lastPoint) >= _updateDistance)
+            {
+                _lastPoint = worldPosition;
+                _currentLine.CreateNewVertex(worldPosition);
+            }
         }
 
         public void StartDrawing(Vector2 startPoint)
         {
-            //_currentLine = Instantiate(_linePrefab, startPoint, Quaternion.identity);
+            Vector2 worldPosition = _camera.ScreenToWorldPoint(startPoint);
+
+            _currentLine = Instantiate(_linePrefab, worldPosition, Quaternion.identity);
+            _currentLine.CreateNewVertex(worldPosition);
+
+            _lastPoint = worldPosition;
             _isDrawing = true;
         }
 
@@ -33,8 +46,6 @@ namespace Pathmaker.Core
         {
             _currentLine = null;
             _isDrawing = false;
-
-            Debug.Log("Stop");
         }
     }
 }
--- Assets/Scripts/Core/Line.cs
+++ Assets/Scripts/Core/Line.cs
@@ -4,6 +4,12 @@ namespace Pathmaker.Core
 {
     public class Line : MonoBehaviour
     {
+        [SerializeField] private LineRenderer _renderer;
 
+        public void CreateNewVertex(Vector2 position)
+        {
+            ++_renderer.positionCount;
+            _renderer.SetPosition(_renderer.positionCount - 1, position);
+        }
     }
 }
